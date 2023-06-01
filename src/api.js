14ac555f42b6ad9f0a6edb1107568ff73a9ab49e
Harsha Kothapalli <saiharsha_kothapalli@srmap.edu.in>
2023-06-01 18:05:37 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'fc2a05f257msh0ac9b6dd59ca97ap11097bjsne60c0ab6ada8',
+		'X-RapidAPI-Key': 'YOUR API RAPID KEY',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -17,4 +17,4 @@ export const geoApiOptions = {
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "b892bec42678dc2a1ff6817bfccbde49";
+export const WEATHER_API_KEY = "your API KEY";
